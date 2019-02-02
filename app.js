8c46875673b467fba3bc25f162600dd3a0aafa75
Benny <benschodie@gmail.com>
2019-02-02 13:57:19 +0100

    add endpoint for styling

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const app = express();
 const fs = require('fs');
 const perfmeter = require('./perfmeter');
 
-const readFileContents = (filename) => new Promise((res, rej) => {
-  fs.readFile(filename, (err, data) => {
-    if (err) rej(err);
-    res(data.toString());
+const readFileContents = filename =>
+  new Promise((res, rej) => {
+    fs.readFile(filename, (err, data) => {
+      if (err) rej(err);
+      res(data.toString());
+    });
   });
-})
 
+app.get('/data', async (req, res) => {
+  console.log(req);
+  const data = await readFileContents('data.json');
+  res.json(JSON.parse(data));
+});
+
+app.get('/frontend', async (req, res) => {
+  res.sendFile(
+    '/Users/benny/Desktop/Sites/puppeteer-website-metrics/index.html'
+  );
+});
 
 app.get('/', async (req, res) => {
   const { url = 'https://google.com' } = req.query;
@@ -18,6 +30,6 @@ app.get('/', async (req, res) => {
   res.json(JSON.parse(data));
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log(`We're ready 🙌  on port 3000!`);
 });
